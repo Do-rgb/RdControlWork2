ff05b13b922ebe47e2522afda00a7592440edc4d
dimigphone@gmail.com <dimigphone@gmail.com>
2020-10-05 21:11:22 +0400

    Добавил немного unit тестов

---
+++ ControlWork/ControlWork.Test/FileSearcherTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using ControlWork.StrFinderConsole;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControlWork.Test
+{
+    [TestClass]
+    public class FileSearcherTests
+    {
+        [TestMethod]
+        public void TryCreate_CorrectInput_TrueAndNotNullObject()
+        {
+            //Arrange
+            //Act
+            var target = FileSearcher.TryCreate("C:\\testFiles\\somefile.txt", "test",out var fileSearcher);
+            //Assert
+            Assert.IsTrue(target);
+            Assert.IsNotNull(fileSearcher);
+        }
+        [TestMethod]
+        public void TryCreate_InCorrectInput_FalseAndNullObject() {
+            //Arrange
+            //Act
+            var target = FileSearcher.TryCreate("C:\\notExistDirectory\\notexistsomefile.txt", "test", out var fileSearcher);
+            //Assert
+            Assert.IsFalse(target);
+            Assert.IsNull(fileSearcher);
+        }
+    }
+}
---
+++ ControlWork/ControlWork.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ControlWork.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ControlWork.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f3d58ff-dc01-48c4-bd3d-b6a192651646")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
